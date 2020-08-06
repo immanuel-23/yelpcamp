c08c2c9784aeb4a6d338c95a0368df2472d8a538
immanuel-23 <62933873+immanuel-23@users.noreply.github.com>
2020-08-06 22:51:38 +0530

    Add files via upload

---
+++ campground.js
@@ -0,0 +1,26 @@
+var mongoose=require("mongoose");
+
+
+var campgroundSchema =new mongoose.Schema({
+	name:String,
+	image:String,
+	description:String,
+	location:String,
+	lat:Number,
+	lng:Number,
+	author:{
+		id:{
+			type:mongoose.Schema.Types.ObjectId,
+			ref:"User"
+		},
+		username:String
+	},
+	comments:[{
+		type: mongoose.Schema.Types.ObjectId,
+		ref:"comment"
+	}]
+	
+});
+
+
+module.exports=mongoose.model("campground",campgroundSchema);
